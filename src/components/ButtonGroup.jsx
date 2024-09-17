db548d3c207abbc8238ac11cc88518fe2612c41f
w0lv3rin3n3 <ene.robert99@gmail.com>
2024-09-17 11:14:52 +0300

    Initial commit

---
+++ src/components/ButtonGroup.jsx
@@ -0,0 +1,27 @@
+import { useItemsStore } from "../stores/itemsStore";
+import Button from "./Button";
+
+export default function ButtonGroup() {
+  const markAllAsComplete = useItemsStore((state) => state.markAllAsComplete);
+  const markAllAsIncomplete = useItemsStore(
+    (state) => state.markAllAsIncomplete
+  );
+  const resetToInitial = useItemsStore((state) => state.resetToInitial);
+  const removeAllItems = useItemsStore((state) => state.removeAllItems);
+  return (
+    <section className="button-group">
+      <Button onClick={markAllAsComplete} buttonType="secondary">
+        Mark all as complete
+      </Button>
+      <Button onClick={markAllAsIncomplete} buttonType="secondary">
+        Mark all as incomplete
+      </Button>
+      <Button onClick={resetToInitial} buttonType="secondary">
+        Reset to initial
+      </Button>
+      <Button onClick={removeAllItems} buttonType="secondary">
+        Remove all items
+      </Button>
+    </section>
+  );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2050. Copyright by w0lv3rin3n3.</small>
+      <small>
+        Version <b>1.5</b>
+      </small>
+    </footer>
+  );
+}
